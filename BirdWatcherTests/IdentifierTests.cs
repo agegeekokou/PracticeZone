f4c5f3e421d85f0ebbd1fab3656df509017f3539
Kokou AGEGEE <delaliage@yahoo.fr>
2025-05-20 07:32:32 +0200

    Unit test Identifier.Clean

---
+++ BirdWatcherTests/IdentifierTests.cs
@@ -0,0 +1,84 @@
+﻿using SqueakyClean;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeZone.UnitTests
+{
+    [TestFixture]
+    public class IdentifierTests
+    {
+        [Test]
+        public void Clean_StringContainsBlankSpaces_ReplaceBlankSpacesWithDash() 
+        {
+            string word = "impos  sible";
+
+            var result = Identifier.Clean(word);
+
+            Assert.That(result, Is.EqualTo("impos__sible"));
+        }
+
+        [Test]
+        public void Clean_StringContainsControlCharacter_ReplaceControlCharacterWithCTRL()
+        {
+            string word = "my\0Id";
+
+            var result = Identifier.Clean(word);
+
+            Assert.That(result, Is.EqualTo("myCTRLId"));
+        }
+
+        [Test]
+        public void Clean_StringIsNull_ReturnString()
+        {
+            string word = null;
+
+            var result = Identifier.Clean(word);
+
+            Assert.That(result, Is.Null);
+        }
+
+
+        [Test]
+        public void Clean_StringIsEmpty_ReturnString()
+        {
+            string word = "";
+
+            var result = Identifier.Clean(word);
+
+            Assert.That(result, Is.Empty);
+        }
+
+        [Test]
+        public void Clean_StringIsKebabCase_ConvertStringToCamelCase()
+        {
+            string word = "my-origin";
+
+            var result = Identifier.Clean(word);
+
+            Assert.That(result, Is.EqualTo("myOrigin"));
+        }
+
+        [Test]
+        public void Clean_NotAllCharactersAreLetters_OmitCharactersThatAreNotLetters()
+        {
+            string word = "1😀2😀3😀";
+
+            var result = Identifier.Clean(word);
+
+            Assert.That(result, Is.EqualTo(""));
+        }
+
+        [Test]
+        public void Clean_StringContainsGreekLowerCase_OmitGreekLowerCaseLetters()
+        {
+            string word = "MyΟβιεγτFinder";
+
+            var result = Identifier.Clean(word);
+
+            Assert.That(result, Is.EqualTo("MyΟFinder"));
+        }
+    }
+}
--- SqueakyClean/Identifier.cs
+++ SqueakyClean/Identifier.cs
@@ -1,112 +1,219 @@
-﻿using System;
-using System.Collections.Generic;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Runtime.InteropServices;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace SqueakyClean
+//{
+//    public class Identifier
+//    {
+//        public static string Clean(string identifier)
+//        {
+//            if (string.IsNullOrEmpty(identifier))
+//                return identifier;
+
+//            string? result = string.Empty;
+
+//            // Change the string in char array
+//            char[] chars = identifier.ToCharArray();
+
+//            if (identifier == "")
+//            {
+//                return "";
+//            }
+//            else if (identifier.Contains(' '))
+//            {
+//                // Loop over the array and change the empty char " " with "_"
+//                for(int i = 0; i < chars.Length; i++)
+//                {
+//                    if (chars[i] == ' ')
+//                    {
+//                        chars[i] = '_';
+//                    }
+//                }
+
+//                // Convert the char array into a string
+//                result = new string(chars);
+//            }
+//            else if (identifier.Contains('\'') || identifier.Contains('\"') || identifier.Contains('\\')
+//                || identifier.Contains('\0') || identifier.Contains('\a') || identifier.Contains('\b')
+//                || identifier.Contains('\f') || identifier.Contains('\n') || identifier.Contains('\r')
+//                || identifier.Contains('\t'))
+
+//            {
+//                for (int i = 0; i < chars.Length; i++)
+//                {
+//                    if ((chars[i] == '\'') || (chars[i] == '\"') || (chars[i] == '\\') || (chars[i] == '\0')
+//                        || (chars[i] == '\a') || (chars[i] == '\b') || (chars[i] == '\f') || (chars[i] == '\n')
+//                        || (chars[i] == '\r') || (chars[i] == '\t'))
+//                    {
+//                        StringBuilder sb = new StringBuilder(identifier);
+
+//                        result = sb.Replace(chars[i].ToString(), "CTRL").ToString();
+//                    }
+//                }              
+//            }
+//            else if (identifier.Contains('-'))
+//            {
+//                StringBuilder sb = new StringBuilder();
+//                bool toUpper = false;
+
+//                foreach (char c in identifier)
+//                {
+//                    if (c == '-')
+//                    {
+//                        toUpper = true;
+//                    }
+//                    else
+//                    {
+//                        sb.Append(toUpper ? char.ToUpper(c) : c);
+//                        toUpper = false;
+//                    }
+//                }
+
+//                result = sb.ToString();
+//            }
+//            else if (!identifier.All(char.IsLetter))
+//            {
+//                StringBuilder sb = new StringBuilder();
+
+//                foreach (char c in identifier)
+//                {
+//                    if (char.IsLetter(c))
+//                    {
+//                        sb.Append(c);
+//                    }
+//                }
+
+//                result = sb.ToString();
+//            }
+//            else if (identifier.All(char.IsLetterOrDigit))
+//            {
+//                StringBuilder sb = new StringBuilder();
+
+//                foreach (char c in identifier)
+//                {
+//                    // Keep letters except Greek lowercase
+//                    if (char.IsLetter(c) && !(c >= '\u03B1' && c <= '\u03C9'))
+//                    {
+//                        sb.Append(c);
+//                    }
+//                }
+
+//                result = sb.ToString();
+//            }
+
+//            Console.WriteLine(result);
+//            return result;
+//        }
+//    }
+//}
+
+using System;
 using System.Linq;
-using System.Runtime.InteropServices;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace SqueakyClean
 {
     public class Identifier
     {
-        public static string Clean(string identifier)
+        public static string? Clean(string? identifier)
         {
-            string? result = string.Empty;
+            if (string.IsNullOrEmpty(identifier))
+                return identifier;
 
-            // Change the string in char array
-            char[] chars = identifier.ToCharArray();
-
-            if (identifier == "")
+            // Handle control characters
+            if (ContainsControlCharacters(identifier))
             {
-                return "";
+                return ReplaceControlCharacters(identifier);
             }
-            else if (identifier.Contains(' '))
+
+            // Replace spaces with underscores
+            if (identifier.Contains(' '))
             {
-                // Loop over the array and change the empty char " " with "_"
-                for(int i = 0; i < chars.Length; i++)
+                return identifier.Replace(' ', '_');
+            }
+
+            // Convert kebab-case to camelCase
+            if (identifier.Contains('-'))
+            {
+                return ConvertKebabToCamel(identifier);
+            }
+
+            // Keep only letters
+            if (!identifier.All(char.IsLetter))
+            {
+                return new string(identifier.Where(char.IsLetter).ToArray());
+            }
+
+            // Remove Greek lowercase letters
+            if (identifier.All(char.IsLetterOrDigit))
+            {
+                return new string(identifier
+                    .Where(c => !(c >= '\u03B1' && c <= '\u03C9'))
+                    .ToArray());
+            }
+
+            return identifier;
+        }
+
+        private static bool ContainsControlCharacters(string input)
+        {
+            foreach (char c in input)
+            {
+                if (c == '\'' || c == '\"' || c == '\\' || c == '\0' ||
+                    c == '\a' || c == '\b' || c == '\f' ||
+                    c == '\n' || c == '\r' || c == '\t')
                 {
-                    if (chars[i] == ' ')
-                    {
-                        chars[i] = '_';
-                    }
+                    return true;
                 }
-
-                // Convert the char array into a string
-                result = new string(chars);
             }
-            else if (identifier.Contains('\'') || identifier.Contains('\"') || identifier.Contains('\\')
-                || identifier.Contains('\0') || identifier.Contains('\a') || identifier.Contains('\b')
-                || identifier.Contains('\f') || identifier.Contains('\n') || identifier.Contains('\r')
-                || identifier.Contains('\t'))
-                
-            {
-                for (int i = 0; i < chars.Length; i++)
-                {
-                    if ((chars[i] == '\'') || (chars[i] == '\"') || (chars[i] == '\\') || (chars[i] == '\0')
-                        || (chars[i] == '\a') || (chars[i] == '\b') || (chars[i] == '\f') || (chars[i] == '\n')
-                        || (chars[i] == '\r') || (chars[i] == '\t'))
-                    {
-                        StringBuilder sb = new StringBuilder(identifier);
-                        
-                        result = sb.Replace(chars[i].ToString(), "CTRL").ToString();
-                    }
-                }              
-            }
-            else if (identifier.Contains('-'))
-            {
-                if (string.IsNullOrEmpty(identifier))
-                    return identifier;
+            return false;
+        }
 
-                StringBuilder sb = new StringBuilder();
-                bool toUpper = false;
+        private static string ReplaceControlCharacters(string input)
+        {
+            StringBuilder sb = new StringBuilder();
 
-                foreach (char c in identifier)
+            foreach (char c in input)
+            {
+                if (c == '\'' || c == '\"' || c == '\\' || c == '\0' ||
+                    c == '\a' || c == '\b' || c == '\f' ||
+                    c == '\n' || c == '\r' || c == '\t')
                 {
-                    if (c == '-')
-                    {
-                        toUpper = true;
-                    }
-                    else
-                    {
-                        sb.Append(toUpper ? char.ToUpper(c) : c);
-                        toUpper = false;
-                    }
+                    sb.Append("CTRL");
                 }
-
-                result = sb.ToString();
-            }
-            else if (!identifier.All(char.IsLetter))
-            {
-                StringBuilder sb = new StringBuilder();
-
-                foreach (char c in identifier)
+                else
                 {
-                    if (char.IsLetter(c))
-                    {
-                        sb.Append(c);
-                    }
+                    sb.Append(c);
                 }
-
-                result = sb.ToString();
             }
-            else if (identifier.All(char.IsLetterOrDigit))
+
+            return sb.ToString();
+        }
+
+        private static string ConvertKebabToCamel(string input)
+        {
+            StringBuilder sb = new StringBuilder();
+            bool toUpper = false;
+
+            foreach (char c in input)
             {
-                StringBuilder sb = new StringBuilder();
-
-                foreach (char c in identifier)
+                if (c == '-')
                 {
-                    // Keep letters except Greek lowercase
-                    if (char.IsLetter(c) && !(c >= '\u03B1' && c <= '\u03C9'))
-                    {
-                        sb.Append(c);
-                    }
+                    toUpper = true;
                 }
-
-                result = sb.ToString();
+                else
+                {
+                    sb.Append(toUpper ? char.ToUpper(c) : c);
+                    toUpper = false;
+                }
             }
-            
-            Console.WriteLine(result);
-            return result;
+
+            return sb.ToString();
         }
     }
 }
+
